dc545fa5cedf4f79879e30245255c35a4befe0c1
sanich123 <aovoronin.piano@gmail.com>
2022-12-17 22:32:55 +0300

    feat: work on routing and structure of project

--- src/js/entry.ts
+++ src/js/entry.ts
@@ -1,4 +1,2 @@
 import "../less/entry.less";
-import { stringNumber } from "./test";
 
-alert(stringNumber("2", 3));
--- src/js/test.ts
+++
@@ -1,3 +0,0 @@
-export function stringNumber(arg1: string, arg2: number) {
-    return arg1 + arg2;
-}
